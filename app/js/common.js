a4f7ead4d5d9563628c78099a01358c5d5110951
panviktar <panviktar.gmail.com>
2018-05-24 16:53:39 +0300

    navbar adaptiv 2

--- app/js/common.js
+++ app/js/common.js
@@ -4,12 +4,23 @@ $(function() {
 		hoverClass: "no-class",
 		delay: 200
 	});
+});
+
+	var mmAPI = $("#my-menu").data( "mmenu" );
+		$("#my-menu").mmenu();
+
+		$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
+	});
+
+
+	
+
+
 	// Custom JS
+	
 
-});
 
-$(".toggle-mnu").click(function() {
-  $(this).toggleClass("on");
-  $(".main-mnu").slideToggle();
-  return false;
-});
+
