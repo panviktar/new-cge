64d4f6b694226a54c20400efa20f774ecce48b2a
panviktar <panviktar.gmail.com>
2018-05-28 16:53:13 +0300

    navbar mobile

--- app/js/common.js
+++ app/js/common.js
@@ -6,14 +6,40 @@ $(function() {
 	});
 });
 
-	var mmAPI = $("#my-menu").data( "mmenu" );
-		$("#my-menu").mmenu();
 
-		$(".toggle-mnu").click(function() {
-		$(this).toggleClass("on");
-		$(".main-mnu").slideToggle();
-		return false;
+$(".sf-menu").after ("<div id='my-menu'>");
+$(".sf-menu").clone().appendTo("#my-menu");
+$("#my-menu").find("*").attr("style", "");
+$("#my-menu").find("ul").removeClass("sf-menu", "");
+$("#my-menu").mmenu({
+	searchfield: {
+		add: true,
+		placeholder: "Поиск по сайту",
+		noResults: "Нет результата"
+	},
+	extensions : [ "widescreen", "theme-white", "effect-menu-slide", "pagedim-black"],
+	navbar: {
+		title: "Меню"
+	}
+});
+
+	var api = $("#my-menu").data("mmenu");
+	api.bind("opened", function() {
+		$(".toggle-mnu").addClass("off");
 	});
+
+		$(".mobile-mnu").click(function() {
+			var mmAPI = $("#my-menu").data( "mmenu" );
+			mmAPI.open();
+			var thiss = $(this).find(".toggle-mnu");
+			thiss.toggleClass("on");
+			$(".main-mnu").slideToggle();
+			return false;
+	});
+
+		
+	
+
 
 
 	
