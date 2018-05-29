65d4bf940724a711fc11dfcd730b9a5ffb3b1ae7
panviktar <panviktar.gmail.com>
2018-05-29 16:53:16 +0300

    left navbar

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,6 @@
-$(function() {
+// Plagin superfish START подключение и стилизация
+
+$(function () {
 	$(".top-line .sf-menu").superfish({
 		cssArrows: false,
 		hoverClass: "no-class",
@@ -6,8 +8,12 @@ $(function() {
 	});
 });
 
+// Plagin superfish END---------------
 
-$(".sf-menu").after ("<div id='my-menu'>");
+// Plagin mmenu START подключение и стилизация
+
+
+$(".sf-menu").after("<div id='my-menu'>");
 $(".sf-menu").clone().appendTo("#my-menu");
 $("#my-menu").find("*").attr("style", "");
 $("#my-menu").find("ul").removeClass("sf-menu", "");
@@ -17,34 +23,34 @@ $("#my-menu").mmenu({
 		placeholder: "Поиск по сайту",
 		noResults: "Нет результата"
 	},
-	extensions: [ "widescreen", "theme-white", "effect-menu-slide", "pagedim-black", "position-right"],
+	extensions: ["widescreen", "theme-white", "effect-menu-slide", "pagedim-black", "position-right"],
 	navbar: {
 		title: '<img src="img/icon/logo.svg" alt="Гродненский областной центр гигиены, эпидемилогии и общественного здоровья"><b>"Гродненский областной ЦГЭОЗ"</b>'
-	}	
+	}
 });
 
-	var api = $("#my-menu").data("mmenu");
-	api.bind("open:start", function() {
-		$(".hamburger").addClass("is-active");
-	}).bind("close:finish", function() {
-		$(".hamburger").removeClass("is-active");
-	})
+// Измение состояния Hamburger
 
-		$(".mobile-mnu").click(function() {
-			var mmAPI = $("#my-menu").data( "mmenu" );
-			mmAPI.open();
-		});
-
-		
-	
+var api = $("#my-menu").data("mmenu");
+api.bind("open:finish", function () {
+	$(".hamburger").addClass("is-active");
+});
+api.bind("close:finish", function () {
+	$(".hamburger").removeClass("is-active");
+})
 
 
+$(".hamburger").click(function () {
+	var mmAPI = $("#my-menu").data("mmenu");
+	mmAPI.open();
+});
 
-	
-
-
-	// Custom JS
-	
+// Plagin mmenu END 
 
 
 
+
+
+
+
+// Custom JS
