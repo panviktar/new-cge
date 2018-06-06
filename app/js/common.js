cccb0862d61ddb68fcb349c98c54b1837880a436
panviktar <panviktar.gmail.com>
2018-06-06 16:54:18 +0300

    s-news

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,6 @@
 // Plagin superfish START подключение и стилизация
-
+// Подключение equalHeights выравнивание контейнера по высоте
 $(function () {
-	
-
 	$(".info-material__title").equalHeights();
 	$(".info-material__text").equalHeights();
 
@@ -177,6 +175,8 @@ function onResize() {
 }onResize();
 window.onresize = function() {onResize()};
 
+
+
 // Custom JS
 
 // BigSlider-------------------------
@@ -275,3 +275,50 @@ navSlider.controller.control = mainSlider;
 foo.onclick = function(){
 	this.classList.toggle('rotate')
 };
+
+// s-news карточки
+
+$(document).ready(function(){
+  var zindex = 10;
+  
+  $("div.card").click(function(e){
+    e.preventDefault();
+
+    var isShowing = false;
+
+    if ($(this).hasClass("show")) {
+      isShowing = true
+    }
+
+    if ($("div.cards").hasClass("showing")) {
+      // a card is already in view
+      $("div.card.show")
+        .removeClass("show");
+
+      if (isShowing) {
+        // this card was showing - reset the grid
+        $("div.cards")
+          .removeClass("showing");
+      } else {
+        // this card isn't showing - get in with it
+        $(this)
+          .css({zIndex: zindex})
+          .addClass("show");
+
+      }
+
+      zindex++;
+
+    } else {
+      // no cards in view
+      $("div.cards")
+        .addClass("showing");
+      $(this)
+        .css({zIndex:zindex})
+        .addClass("show");
+
+      zindex++;
+    }
+    
+  });
+});
