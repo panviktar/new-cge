470370e7384acc062d7d1df05c24acbbb7380453
panviktar <panviktar.gmail.com>
2018-06-13 16:52:07 +0300

    переделал сетку

--- app/js/common.js
+++ app/js/common.js
@@ -148,15 +148,15 @@ responsive: {
 	},
 
 768: {
-	items: 3
+	items: 2
 },
 
 990: {
-	items: 4
+	items: 3
 },
 
 1200: {
-	items: 5
+	items: 4
 }
 }
 });
@@ -186,7 +186,7 @@ responsive: {
 	items: 4
 },
 1200: {
-	items: 5
+	items: 4
 }
 }
 });
